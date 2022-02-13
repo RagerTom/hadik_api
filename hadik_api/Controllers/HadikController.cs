deabf6d07e96741efb021fa5859dceb432479b8e
RagerTom <tomas.moravcik04@gmail.com>
2022-02-13 12:08:12 +0100

    Add project files.

---
+++ hadik_api/Controllers/HadikController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hadik_api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class HadikController : ControllerBase
+    {
+
+        private static List<Hadik> hadikData = new List<Hadik>
+        {
+           
+        };
+        [HttpGet]
+        [Route("GetHadikData")]
+        public async Task<ActionResult<List<Hadik>>> GetHadikData()
+        {
+
+
+            return Ok(hadikData);
+        }
+
+        [HttpPost]
+        [Route("PostHadikData")]
+        public async Task<ActionResult<List<Hadik>>> AddhadikData(Hadik api)
+        {
+
+            hadikData.Insert(0, api);
+            return Ok(hadikData);
+        }
+    }   
+}
---
+++ hadik_api/Hadik.cs
@@ -0,0 +1,25 @@
+﻿namespace hadik_api
+{
+    public class Hadik
+    {
+        public int SavedScore { get; set; }
+    
+        public int SavedSnakeLenght { get; set; }
+
+        public int SavedSnakePosX { get; set; }
+
+        public int SavedSnakePosY { get; set; }
+
+        public int SavedVelocityX { get; set; }
+
+        public int SavedVelocityY { get; set; }
+
+        public IEnumerable<HadikParts> SavedTail { get; set; }
+    }
+
+    public record HadikParts
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
